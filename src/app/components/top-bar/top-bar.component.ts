fe611f009bdef61897dfc42db8a4b4136cf2fc02
Iuri Oliveira <80722767+IuriOliveira7@users.noreply.github.com>
2023-12-01 21:02:18 -0300

    perfil_button_sair

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class TopBarComponent {
 
+  constructor(private router: Router) {}
+
+  sair() {
+    this.router.navigate(['/']);
+  }
 }
