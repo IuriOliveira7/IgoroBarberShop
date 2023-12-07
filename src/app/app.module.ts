8bbcb530b33d57459c56470e388c09a5678b2fc1
Iuri Oliveira <80722767+IuriOliveira7@users.noreply.github.com>
2023-12-07 18:56:04 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-
 import { ToastrModule } from 'ngx-toastr';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -12,6 +11,7 @@ import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { ConteudoHomeComponent } from './components/conteudo-home/conteudo-home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PageInicioComponent } from './pages/page-inicio/page-inicio.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
