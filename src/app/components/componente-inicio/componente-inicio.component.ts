6a3b1f5c5380aa216d127809b4fd2bda097d69a6
Iuri Oliveira <80722767+IuriOliveira7@users.noreply.github.com>
2023-12-13 23:12:11 -0300

    att_delet_user_fixo

--- src/app/components/componente-inicio/componente-inicio.component.ts
+++ src/app/components/componente-inicio/componente-inicio.component.ts
@@ -88,8 +88,8 @@ export class ComponenteInicioComponent {
   // LOGIN/CADASTRO
 
   novasenha: string = '';
-  email: string = 'iuriolivera7@gmail.com';
-  senha: string = '123456789';
+  email: string = '';
+  senha: string = '';
   usuario: string = '';
   telefone: string = '';
   confirmasenha: string = '';
